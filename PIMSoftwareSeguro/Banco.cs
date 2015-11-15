fceb7e52c8f451e0549d4a0567f42b477ee59c10
guilhermekbral <guilhermevannuchi@gmail.com>
2015-11-15 20:40:54 -0200

    Criada classe de conexao com o banco de dados

---
+++ PIMSoftwareSeguro/Banco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace PIMSoftwareSeguro
+{
+    public class Banco
+    {
+        private MySqlConnection banco;
+
+        public void conectar()
+            {
+
+                banco = new MySqlConnection("Persist Security Info=false; SERVER=localhost;DATABASE=softseguros;UID=root;PWD=123456");
+                banco.Open();
+
+            }
+
+    }
+}
